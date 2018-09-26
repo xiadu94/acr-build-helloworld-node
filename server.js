a0f922549395a2dc39b9187ebde5615148a837de
Xian Du <xiadu@microsoft.com>
2018-09-26 16:55:49 -0700

    test commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 80
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
+  response.write('Hello World!\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
